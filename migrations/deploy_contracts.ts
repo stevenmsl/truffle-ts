fb651b601328412e1137ed985353556b44ccbbc5
Steven Lin <steven_lx@outlook.com>
2021-09-22 10:49:20 -0700

    fix npm scripts

--- migrations/deploy_contracts.ts
+++ migrations/deploy_contracts.ts
@@ -1,24 +1,22 @@
-
 type Network = "development" | "test";
 
 /* TA-03 */
-module.exports = (artifacts: any /* #TA-04 */ , web3: Web3) => {
-    return async (
-        deployer: Truffle.Deployer,
-        network: Network,
-        accounts: Truffle.Accounts
-    ) => {
-        const ConvertLib = artifacts.require("ConvertLib");
-        const MetaCoin = artifacts.require("MetaCoin");
-        
-        deployer.deploy(ConvertLib);
-        deployer.link(ConvertLib, MetaCoin);
-        deployer.deploy(MetaCoin);
+module.exports = (artifacts: Truffle.Artifacts, web3: Web3) => {
+  return async (
+    deployer: Truffle.Deployer,
+    network: Network,
+    accounts: Truffle.Accounts
+  ) => {
+    const ConvertLib = artifacts.require("ConvertLib");
+    const MetaCoin = artifacts.require("MetaCoin");
 
-        const metaCoin = await MetaCoin.deployed();
-        console.log(
-            `Metacoin deployed at ${metaCoin.address} in network: ${network}.`
-        );
-    };
- 
-}; 
+    deployer.deploy(ConvertLib);
+    deployer.link(ConvertLib, MetaCoin);
+    deployer.deploy(MetaCoin);
+
+    const metaCoin = await MetaCoin.deployed();
+    console.log(
+      `Metacoin deployed at ${metaCoin.address} in network: ${network}.`
+    );
+  };
+};
